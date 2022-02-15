fbf0b5d588db328d2f0db443c734fe98deb4b78f
2jaebbang <2jaebbang@naver.com>
2022-02-15 19:41:28 +0900

    feat: 초기 폴더 구조 & 라우터 세팅

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import Login from 'pages/Login';
+import SignUp from 'pages/SignUp';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
