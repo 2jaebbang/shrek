c6f3cf55cf5c184e5ce57fffa36a45400e648530
2jaebbang <2jaebbang@naver.com>
2022-02-17 02:23:09 +0900

    feat: 로그인 useQuery 적용

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import loadable from '@loadable/component';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Login = loadable(() => import('pages/Login'));
 const SignUp = loadable(() => import('pages/SignUp'));
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signUp" element={<SignUp />}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signUp" element={<SignUp />}></Route>
+        </Routes>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,21 +3,28 @@ import useInput from 'hooks/useInput';
 import { Button, Error, Form, Header, Input, Label, LinkContainer } from 'pages/SignUp/styles';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useQuery } from 'react-query';
+import fetcher from 'utils/fetcher';
 
 const LogIn = () => {
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
   const [logInError, setLogInError] = useState(false);
-
+  //내 로그인 정보
+  const { data, error } = useQuery(['user'], () => fetcher('http://localhost:3095/api/users'));
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
       setLogInError(false);
       axios
-        .post('/users/login', {
-          email,
-          password,
-        })
+        .post(
+          'http://localhost:3095/api/users/login',
+          {
+            email,
+            password,
+          },
+          { withCredentials: true },
+        )
         .then((response) => {
           console.log(response.data);
         })
