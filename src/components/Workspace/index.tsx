8abf98339a7ccfe7305e747b7d78b533f4bc2c9d
2jaebbang <2jaebbang@naver.com>
2022-02-18 00:39:39 +0900

    feat: 워크스페이스 초기 컴포넌트 UI 추가

--- src/components/Workspace/index.tsx
+++ src/components/Workspace/index.tsx
@@ -3,6 +3,18 @@ import React, { FC, useCallback } from 'react';
 import { Navigate } from 'react-router';
 import useSWR from 'swr';
 import fetcher from 'utils/fetcher';
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from './styles';
+import gravatar from 'gravatar';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher, { dedupingInterval: 10000 });
@@ -18,7 +30,21 @@ const Workspace: FC = ({ children }) => {
   }
   return (
     <div>
+      <Header>
+        <RightMenu />
+        <span>
+          <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.email} />
+        </span>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Shrek</WorkspaceName>
+          <MenuScroll>MenuScorll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
