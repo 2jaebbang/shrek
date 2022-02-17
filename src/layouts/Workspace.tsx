3f53db031c22d464192c0710c4b27a31c69c70c3
2jaebbang <2jaebbang@naver.com>
2022-02-17 17:24:40 +0900

    feat: 워크스페이스 추가

---
+++ src/layouts/Workspace.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import React, { FC, useCallback } from 'react';
+import useSWR from 'swr';
+import fetcher from 'utils/fetcher';
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, revalidate } = useSWR('http://localhost:3095/api/users', fetcher, { dedupingInterval: 10000 });
+  const onLogout = useCallback(() => {
+    axios.post('http://localhost:3095/api/users/logout', null, { withCredentials: true });
+  }, []);
+  return (
+    <div>
+      <button onClick={onLogout}>로그아웃</button>
+    </div>
+  );
+};
+
+export default Workspace;
